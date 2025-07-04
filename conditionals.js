7c501d29a297898757fb74bd7badaf247400c966
Sagar Bhandari <rsaagar7200@gmail.com>
2025-07-04 06:37:24 +0545

    if/else if /else

---
+++ conditionals.js
@@ -0,0 +1,27 @@
+// control flow
+
+// control statements
+
+// conditional  blocks
+// decision making
+
+// if (age > 17) {
+//   console.log("you are adult");
+// } else {
+//   console.log("you are minor");
+// }
+
+// else if
+const age = 99;
+
+if (age <= 17) {
+  console.log("you are minor");
+} else if (age > 17 && age <= 30) {
+  console.log("you are adult");
+} else if (age > 30 && age < 60) {
+  console.log("you are senior");
+} else {
+  console.log("major");
+}
+
+// loops
